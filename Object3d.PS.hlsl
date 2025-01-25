f61174e0cb79a5ce1abebee1cfcc910f977aa6b5
komuro-ryuhei <k023g0040@g.neec.ac.jp>
2025-01-26 00:47:13 +0900

    02_01

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -53,8 +53,13 @@ PixelShaderOutput main(VertexShaderOutput input)
         
         float3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
         
+        float3 halfVector = normalize(-gDirectionalLight.direction + toEye);
+        
+        float NDotH = dot(normalize(input.normal), halfVector);
+        
         float RdotE = dot(reflectLight, toEye);
-        float specularPow = pow(saturate(RdotE), gMaterial.shininess);
+        
+        float specularPow = pow(saturate(NDotH), gMaterial.shininess);
         
         // 拡散反射
         float3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
